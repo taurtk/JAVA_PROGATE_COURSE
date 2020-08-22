d4b813e03dc4cf1dae4370231463ea29b0cd7b17
Taufique Khan <36895499+taurtk@users.noreply.github.com>
2020-08-22 11:48:00 +0545

    Create Project_1.java

---
+++ Project_1.java
@@ -0,0 +1,28 @@
+//BISMILLAH-HIR-RAHMANIR-RAHIM
+//Taufique Khan
+class Main {
+  public static void main(String[] args) {
+    // Assign a string to the name variable
+    String name="Taufique";
+    
+    // Assign an integer to the age variable
+    int age = 22;
+    
+    // Assign a double to the height variable (i.e. 1.6)
+    double height = 1.7;
+    
+    // Assign a double to the weight variable (i.e. 50.0)
+    double weight = 50.0;
+    
+    // Calculate the BMI and assign it into the bmi variable
+    double bmi = weight / height / height;
+    
+    // Print name, age, height, weight, and BMI
+    System.out.println("My name is "+name);
+    System.out.println("I am "+age+" years old");
+    System.out.println("My height is "+ height+" meters");
+    System.out.println("My weight is"+weight+ "kilograms");
+    System.out.println("My BMI is "+bmi);
+    
+  }
+}
